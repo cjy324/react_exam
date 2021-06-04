15ca8bb6fb8f2978b5106b460c1a4c888d1f55db
cjy324 <juy32400@naver.com>
2021-06-04 21:34:11 +0900

    21.06.04 uesEffect() Hook 적용

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,7 +1,8 @@
-import dummy from '../db/data.json';
+//import dummy from '../db/data.json';   //json-server로 API연결로 dummy 필요 x
 // URL에 포함된 값을 얻기 위해 react-router-dom에서 제공하는 'useParams'를 사용
 import { useParams } from 'react-router-dom';
 import Word from './Word';
+import { useEffect, useState } from 'react';
 
 export default function Day(){
 
@@ -9,15 +10,27 @@ export default function Day(){
   //const day = useParams().day;  
   //2번 방식
   const {day} = useParams();
-                                                          //숫자로 형변환
-  const wordList = dummy.words.filter(word => word.day === Number(day));
+  //json-server로 API연결로 dummy 필요 x                     //숫자로 형변환
+  // const wordList = dummy.words.filter(word => word.day === Number(day));
+
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      setWords(data)
+    })
+  }, [day])
 
   return(
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map(word => (
+          {words.map(word => (
             //'Word'컴포넌트에 word값과 key값을 props로 전달
             <Word word={word} key={word.id}/>
           ))}
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import dummy from '../db/data.json';
+// import dummy from '../db/data.json';  //json-server로 API연결
+
+
 
 export default function DayList(){
+
+  const [days, setDays] = useState([])
+
+  // useEffect() hook?
+  // 어떤 상태 값이 바뀌었을때 동작하게 할 수 있는 함수
+  // 어떤 상태 값이 변할때 다시 렌더링되는 방식
+  // vue에서 watch?와 비슷한 개념?
+  // useEffect(함수,매개변수2)
+  // 두번째 매개변수를 넣어주지않아도 동작은 하지만 불필요하게 계속 함수가 호출됨
+  // 두번째 매개변수를 넣어주면 그 매개변수의 값이 변할때만 함수가 호출됨
+  useEffect(() => {
+    //API 비동기 통신을 위해 'fetch()'를 적용
+    fetch(`http://localhost:3001/days`)
+    .then(res => {
+      return res.json()
+      //이렇게 하면 받은값(res)이 json으로 변환되고 promise를 반환함
+    })
+    .then(data => {
+      setDays(data)
+    })
+  }, [])
+  //이렇게 빈 배열을 두번째 매개변수로 넣어주게 되면
+  //렌더링 후 처음 한번만 함수가 호출되어 불필요한 호출을 줄일 수 있음
+
   return(
     <>
       <ul className="list_day">
-        {dummy.days.map(day => (
+        {days.map(day => (
           <li key={day.id}>
             {/* Link는 랜더링 시 a태그(+ href)로 랜더링된다 */}
             <Link to={`/day/${day.day}`}>
